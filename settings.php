ac382e9d9ce6b165b07c8e41b66a8978c79ccaf4
wojtij99 <47479648+wojtij99@users.noreply.github.com>
2023-06-15 21:59:51 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,17 +1,3 @@
 <?php
-	$host = "localhost";
-	$db_user = "root";
-	$db_password = "Tradycyjne78!";
-	$db_name = "";
-
-	$head = "<meta charset='utf-8' />
-	<meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1' />
-	
-	<title>Wojciech Jędrzejewski</title>
-	
-	<meta name='desrciption' content='' />
-	<meta name='keywords' content='' />
-	<meta name='author' content='Wojciech Jędrzejewski'/>
-	<link rel='stylesheet' href='css/main.css' type='text/css' />
-	";
+//NOTHING
 ?>
